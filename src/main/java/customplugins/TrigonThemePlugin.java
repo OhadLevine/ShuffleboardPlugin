5b03a9becb1f8018318788232c5901a862fb6759
Ohad <ohadlevin99@gmail.com>
2020-02-05 18:08:42 +0200

    frixed issues

---
+++ src/main/java/customplugins/TrigonThemePlugin.java
@@ -0,0 +1,17 @@
+package customplugins;
+
+import edu.wpi.first.shuffleboard.api.plugin.Description;
+import edu.wpi.first.shuffleboard.api.plugin.Plugin;
+import edu.wpi.first.shuffleboard.api.theme.Theme;
+
+import java.util.List;
+
+
+@Description(
+    group = "Trigon.5990",
+    name = "Trigon Theme",
+    version = "1.0.0",
+    summary = "A plugin for changing the shuffleboard colors to trigons colors")
+public class TrigonThemePlugin extends Plugin {
+
+}
