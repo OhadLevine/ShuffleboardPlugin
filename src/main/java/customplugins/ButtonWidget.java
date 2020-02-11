0bc1852cd561647460b1e37b978bbdbfc54d11fb
Ohad <ohadlevin99@gmail.com>
2020-02-11 17:11:11 +0200

    built code and continued following tutorial

--- src/main/java/customplugins/ButtonWidget.java
+++ src/main/java/customplugins/ButtonWidget.java
@@ -1,13 +1,14 @@
 package customplugins;
 
-
 import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
 @Description(dataTypes = Boolean.class, name = "my test widget")
-public final class buttonWidget extends SimpleAnnotatedWidget {
+public final class ButtonWidget extends SimpleAnnotatedWidget {
 
+    @FXML
     protected Pane buttonWidgetPane;
 
     public Pane getView() {
